f4d7ac774ac728ac19adfd708313f0acae625ee4
almahmudzihad <amzihad@gmail.com>
2026-04-19 14:49:13 +0600

    some edit text

--- src/app/dashboard/demo/page.jsx
+++ src/app/dashboard/demo/page.jsx
@@ -11,8 +11,7 @@ function page() {
         <div className="card-body">
           <h2 className="card-title">Card Title</h2>
           <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
+            If a dog chews shoes whose shoes does he choose?
           </p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
